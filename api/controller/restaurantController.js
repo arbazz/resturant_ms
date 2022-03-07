ca4bc420327cad753b4e3f8efea3340b32d14744
MuhammadHamzaFarooq <mhamza2021999@gmail.com>
2022-03-07 14:33:49 +0500

    add searchChildData function by hamza dev

--- api/controller/restaurantController.js
+++ api/controller/restaurantController.js
@@ -1,13 +1,12 @@
 const fs = require("fs");
+const { ObjectId } = require("mongodb");
+const Restaurantdata = require("../../model/Restaurantdata");
 
 const resturantController = {
   mockGetAllRecipes: async (req, res) => {
     try {
-      const file = await fs.readFileSync(
-        "./controller/onboarding/recipes.json",
-        "utf8"
-      );
-      let obj = JSON.parse(data);
+      const file = await fs.readFileSync("./config/recipes.json", "utf8");
+      let obj = JSON.parse(file);
       let arr = [];
       arr = arr.push(obj);
       const returnObj = {
@@ -27,91 +26,241 @@ const resturantController = {
     }
   },
   searchData: async (req, res) => {
-    if (
-      Object.values(req.query).every((page) => page === "") ||
-      (Object.keys(req.query).length === 0) === true ||
-      req.query.type === "" ||
-      req.query.keyword === ""
-    ) {
-      //return boolean
-      return res.status(400).json({
-        status: false,
-        msg: "Bad Input",
-      });
-    }
-    let query = {};
-    if (req.query.type === "restaurant") {
-      (query = {
-        $or: [
-          {
-            restaurantName: {
-              $regex: req.query.keyword,
-              $options: "si",
+    try {
+      if (
+        Object.values(req.query).every((page) => page === "") ||
+        (Object.keys(req.query).length === 0) === true ||
+        req.query.type === "" ||
+        req.query.keyword === ""
+      ) {
+        //return boolean
+        return res.status(400).json({
+          status: false,
+          msg: "Bad Input",
+        });
+      }
+      let query1 = {};
+      let query2 = {};
+      if (req.query.type === "restaurant") {
+        query1 = {
+          $or: [
+            {
+              restaurantName: {
+                $regex: req.query.keyword,
+                $options: "si",
+              },
             },
-          },
-          {
-            dishes: {
-              $elemMatch: {
-                resDishName: {
-                  $regex: req.query.keyword,
-                  $options: "si",
+            {
+              dishes: {
+                $elemMatch: {
+                  resDishName: {
+                    $regex: req.query.keyword,
+                    $options: "si",
+                  },
                 },
               },
             },
-          },
-        ],
-      }),
-        {
+          ],
+        };
+        query2 = {
           restaurantName: 1,
           restaurantID: 1,
           "image.imageUrl": 1,
           _id: 1,
         };
-    } else if (req.query.type === "dish") {
-      query = {
-        dishName: {
-          $elemMatch: {
-            name: {
-              $regex: req.query.keyword,
-              $options: "si",
+      } else if (req.query.type === "dish") {
+        query1 = {
+          dishName: {
+            $elemMatch: {
+              name: {
+                $regex: req.query.keyword,
+                $options: "si",
+              },
             },
           },
-        },
-      };
-    }
-  },
-
-  searchChildData: async (req, res) => {
-    // if true then req.query is empty
-    if (
-      Object.values(req.query).every((page) => page === "") ||
-      (Object.keys(req.query).length === 0) === true ||
-      req.query.type === "" ||
-      req.query.keyword === "" ||
-      req.query.resid === ""
-    ) {
-      //return boolean
-      return res.status(400).json({
+        };
+        query2 = {
+          "dishName.$": 1,
+          image: 1,
+          _id: 1,
+        };
+      } else if (req.query.type === "recipes") {
+        query1 = {
+          dishName: {
+            $elemMatch: {
+              name: {
+                $regex: req.query.keyword,
+                $options: "si",
+              },
+            },
+          },
+        };
+        query2 = {
+          "dishName.$": 1,
+          image: 1,
+          _id: 1,
+        };
+      }
+      let response = await Recipes.find({ query1, query2 });
+      return res.status(200).json({
+        status: true,
+        response,
+      });
+    } catch (error) {
+      return res.status(500).json({
         status: false,
-        msg: "Bad Input",
+        msg: "Something Went Wrong!!!",
+        err: err,
       });
     }
-    //  console.log(req.query.type );
-    if (req.query.type === "popular") {
-      const data = await fs.readFileSync("./config/prioritizeArr.json", "utf8");
-      console.log(data);
-      res.send("popular data" + data);
-    }
-    obj = JSON.parse(data);
-    let arrayData = [];
-    obj.map((item, index) => {
-      //arrayData[index] = new ObjectId(item.recipeId);
-      arrayData[index] = item.recipeId;
-      if (obj.length === index + 1) {
-        return arrayData;
+  },
+  searchChildData: async (req, res) => {
+    try {
+      // if true then req.query is empty
+      if (
+        Object.values(req.query).every((page) => page === "") ||
+        (Object.keys(req.query).length === 0) === true ||
+        req.query.type === "" ||
+        req.query.keyword === "" ||
+        req.query.resid === ""
+      ) {
+        //return boolean
+        return res.status(400).json({
+          status: false,
+          msg: "Bad Input",
+        });
       }
-    });
-    console.log(arrayData);
+      //  console.log(req.query.type );
+      if (req.query.type === "popular") {
+      
+        const data = await fs.readFileSync(
+          "./config/prioritizeArr.json",
+          "utf8"
+        );
+
+        let obj = JSON.parse(data);
+        let arrayData = [];
+        obj.map((item, index) => {
+          //arrayData[index] = new ObjectId(item.recipeId);
+          arrayData[index] = item.recipeId;
+          if (obj.length === index + 1) {
+            return arrayData;
+          }
+        });
+
+        let findData;
+        if (req.query.keyword == "all") {
+          findData = {
+            $and: [
+              {
+                _id: ObjectId(req.query.resid),
+              },
+              {
+                "restaurant_menu.recipeId": {
+                  $in: arrayData,
+                },
+              },
+            ],
+          };
+        } else {
+          findData = {
+            $and: [
+              {
+                _id: ObjectId(req.query.resid),
+              },
+              {
+                "restaurant_menu.recipeId": {
+                  $in: arrayData,
+                },
+              },
+              {
+                "restaurant_menu.dish_name": {
+                  $regex: req.query.keyword,
+                  $options: "si",
+                },
+              },
+            ],
+          };
+        }
+
+        let resultUsersData = await Restaurantdata.aggregate([
+          {
+            $unwind: "$restaurant_menu",
+          },
+          {
+            $match: findData,
+          },
+          {
+            $project: {
+              _id: 1,
+              //restaurantTags: 1,
+              image: 1,
+              restaurantName: 1,
+              //cuisineServed: 1,
+              pricing_details: 1,
+              contact_details: 1,
+              "restaurant_menu.meal_courses": 1,
+              "restaurant_menu.typeOfMenu": 1,
+              "restaurant_menu.dish_name": 1,
+              //"restaurant_menu.resDishUrl": 1,
+              "restaurant_menu.price": 1,
+              //"restaurant_menu.dishSubstitute": 1,
+              //"restaurant_menu.dishOptions": 1,
+              //"restaurant_menu.dishSize": 1,
+              //"restaurant_menu.dishAddOn": 1,
+              "restaurant_menu.image": 1,
+            },
+          },
+        ]);
+        if (resultUsersData) {
+          if (resultUsersData.length > 0) {
+            const returnObj = {
+              restaurantId: resultUsersData[0]._id,
+              restaurantImg: resultUsersData[0].image,
+              restaurantName: resultUsersData[0].restaurantName,
+              //restaurantCuisine: resultUsersData[0].cuisineServed,
+              restaurantPricing: resultUsersData[0].pricing_details,
+              restaurantAddress: resultUsersData[0].contact_details,
+              //restaurantTags: resultUsersData[0].restaurantTags,
+            };
+
+            let dishes = resultUsersData
+              .filter(
+                (data) => req.query.resid.toString() === data._id.toString()
+              )
+              .map((d) => d.restaurant_menu);
+            let removeDup = dishes.reduce((unique, o) => {
+              if (
+                !unique.some(
+                  (obj) =>
+                    obj.dish_name === o.dish_name && obj.price === o.price
+                )
+              ) {
+                unique.push(o);
+              }
+              return unique;
+            }, []);
+          }
+        }
+        if (data.data === null) {
+          res.status(200).json({
+            status: true,
+            data: null,
+          });
+        } else {
+          res.status(200).json({
+            status: true,
+            data,
+          });
+        }
+      }
+      
+    } catch (error) {
+      return res.status(500).send({
+        message: "Some Thing went wrong",
+        err: error.message,
+      });
+    }
   },
 };
 
