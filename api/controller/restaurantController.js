88995403b09a27b89722723aa699eb781a20c72d
Arbaz <tauseef.arbaz@pikky.io>
2022-03-09 15:38:31 +0500

    fixes?:

--- api/controller/restaurantController.js
+++ api/controller/restaurantController.js
@@ -142,33 +142,6 @@ const resturantController = {
           "utf8"
         );
 
-<<<<<<< HEAD
-    if (
-      Object.values(req.query).every((page) => page === "") ||
-      (Object.keys(req.query).length === 0) === true ||
-      req.query.type === "" ||
-      req.query.keyword === "" ||
-      req.query.resid === ""
-    ) {
-      //return boolean
-      return res.status(400).json({
-        status: false,
-        msg: "Bad Input",
-      });
-    }
-    let findData = {};
-    if (req.query.type === "popular") {
-      let file = await fs.readFileSync(
-        "./config/prioritizeArr.json",
-        "utf8")
-      obj = JSON.parse(file);
-      let arrayData = [];
-      obj.map((item, index) => {
-        //arrayData[index] = new ObjectId(item.recipeId);
-        arrayData[index] = (item.recipeId);
-        if (obj.length === index + 1) {
-          return arrayData;
-=======
         let obj = JSON.parse(data);
         let arrayData = [];
         obj.map((item, index) => {
@@ -212,7 +185,6 @@ const resturantController = {
               },
             ],
           };
->>>>>>> 2686c77be4993e133339b1baf893e30a01f424df
         }
 
         let resultUsersData = await Restaurantdata.aggregate([
@@ -280,177 +252,6 @@ const resturantController = {
             data: null,
           };
         }
-<<<<<<< HEAD
-      }
-    } else if (
-      req.query.mainCategory != "all" &&
-      req.query.subCategory == "all"
-    ) {
-      if (req.query.keyword == "all") {
-        findData = {
-          "_id": ObjectId(req.query.resid),
-          "restaurant_menu.meal_courses": {
-            $regex: req.query.mainCategory,
-            $options: "si",
-          },
-        };
-      } else {
-        findData = {
-          $and: [{
-            "_id": ObjectId(req.query.resid)
-          },
-          {
-            "restaurant_menu.meal_courses": {
-              $regex: req.query.mainCategory,
-              $options: "si",
-            },
-          },
-          {
-            "restaurant_menu.dish_name": {
-              $regex: req.query.keyword,
-              $options: "si",
-            },
-          },
-          ],
-        };
-      }
-    } else if (
-      req.query.mainCategory != "all" &&
-      req.query.subCategory != "all"
-    ) {
-      // check when click courses and click parent category Breakfast, lunch, bunch dinner and click sub category like drinks staters etc
-      if (req.query.keyword == "all") {
-        findData = {
-          $and: [{
-            "restaurant_menu.meal_courses": {
-              $regex: req.query.mainCategory,
-              $options: "si",
-            },
-          },
-          {
-            "restaurant_menu.typeOfMenu": {
-              $regex: req.query.subCategory,
-              $options: "si",
-            },
-          },
-          ],
-        };
-      } else {
-        findData = {
-          $and: [{
-            "restaurant_menu.meal_courses": {
-              $regex: req.query.mainCategory,
-              $options: "si",
-            },
-          },
-          {
-            "restaurant_menu.typeOfMenu": {
-              $regex: req.query.subCategory,
-              $options: "si",
-            },
-          },
-          {
-            "restaurant_menu.dish_name": {
-              $regex: req.query.keyword,
-              $options: "si",
-            },
-          },
-          ],
-        };
-      }
-    } // else end
-
-    if (req.query.keyword == "all") {
-      keyWordSerch = {};
-    } else {
-      keyWordSerch = {
-        restaurant_menu: {
-          $elemMatch: {
-            dish_name: {
-              $regex: req.query.keyword,
-              $options: "si",
-            },
-          },
-        },
-      };
-    }
-    await Restaurantdata.aggregate(
-      [{
-        $unwind: "$restaurant_menu"
-      },
-      {
-        $match: findData
-      },
-      {
-        $project: {
-          _id: 1,
-          //restaurantTags: 1,
-          "image": 1,
-          restaurantName: 1,
-          //cuisineServed: 1,
-          pricing_details: 1,
-          contact_details: 1,
-          "restaurant_menu.meal_courses": 1,
-          "restaurant_menu.typeOfMenu": 1,
-          "restaurant_menu.dish_name": 1,
-          //"restaurant_menu.resDishUrl": 1,
-          "restaurant_menu.price": 1,
-          //"restaurant_menu.dishSubstitute": 1,
-          //"restaurant_menu.dishOptions": 1,
-          //"restaurant_menu.dishSize": 1,
-          //"restaurant_menu.dishAddOn": 1,
-          "restaurant_menu.image": 1,
-        },
-      },
-      ]).then((result) => {
-        result.map((d) => {
-          const returnObj = {
-            restaurantId: resultUsersData[d]._id,
-            restaurantImg: resultUsersData[d].image,
-            restaurantName: resultUsersData[d].restaurantName,
-            //restaurantCuisine: resultUsersData[0].cuisineServed,
-            restaurantPricing: resultUsersData[d].pricing_details,
-            restaurantAddress: resultUsersData[d].contact_details,
-            //restaurantTags: resultUsersData[0].restaurantTags,
-          };
-
-          let dishes = result
-            .filter(
-              (data) => req.query.resid.toString() === data._id.toString()
-            )
-            .map((d) => d.restaurant_menu);
-          let removeDup = dishes.reduce((unique, o) => {
-            if (
-              !unique.some(
-                (obj) =>
-                  obj.dish_name === o.dish_name &&
-                  obj.price === o.price
-              )
-            ) {
-              unique.push(o);
-            }
-            return unique;
-          }, []);
-
-          return {
-            ...returnObj,
-            dishes: removeDup,
-          }
-      })
-      return res.status(200).json({
-        status: true,
-        data,
-      });
-    })
-        .catch(() => {
-          return res.status(500).json({
-            status: false,
-            msg: "Something Went Wrong!!!",
-          });
-        });
-      },                     
-  };
-=======
 
         if (dataObj.data === null) {
           res.status(200).json({
@@ -987,6 +788,5 @@ const resturantController = {
     }
   },
 };
->>>>>>> 2686c77be4993e133339b1baf893e30a01f424df
 
-  module.exports = resturantController;
+module.exports = resturantController;
